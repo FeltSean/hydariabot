ec9c718bdb93c87c7910bf39c62a3ed22932388b
FeltSean <37269943+FeltSean@users.noreply.github.com>
2019-06-09 22:41:55 +0200

    Create say.js

---
+++ .gitignore/say.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission('MANAGE_MESSAGES'))
+    return message.channels.send(
+        "Vous n'avez pas la permission pour faire celà !!!"
+    );
+let messageToBot = args.join(".");
+message.delete();
+message.channel.send(messageToBot);
+};
+
+modules.exports.help = {
+    command: "say"
+};
