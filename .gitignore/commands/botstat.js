5cb75173d76c1ca2e0a33039e36c71b7e0bbb5de
FeltSean <37269943+FeltSean@users.noreply.github.com>
2019-10-19 19:17:12 +0200

    Add files via upload

--- .gitignore/commands/botstat.js
+++ .gitignore/commands/botstat.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message) => {
 
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`Tu n'as pas le droit d'éxécuter cette commande !!!`)
-        message.channel.send("Hey,\n**HydariaBot** : En ligne\n**Koya** : En ligne\n**Mee6** : En ligne\n**Rythm** : En ligne\n**RaidProtect** : En ligne\n**GiveawayBot** : En ligne")
+        message.channel.send("Hey,\n**HydariaBot** : En ligne\n**Koya** : En ligne\n**Mee6** : En ligne\n**Rythm** : En ligne\n**GiveawayBot** : En ligne\n**YAGPDB** : En ligne")
         console.log("commande de botstats demandé")
         message.delete();
 }
